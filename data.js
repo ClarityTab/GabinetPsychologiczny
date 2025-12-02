57f0e0ede06a776e16f4243889b4a7bb4285822c
ClarityTab <callitaplan.planit@gmail.com>
2025-12-02 17:54:10 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,204 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-poczekalniaprzedpokj",
+      "name": "Poczekalnia/przedpokój",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.7794684087972392,
+        "pitch": 0.11219868498786312,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.39271228374332,
+          "pitch": 0.026321434404481536,
+          "rotation": 0,
+          "target": "3-toaleta"
+        },
+        {
+          "yaw": 0.6173504233025149,
+          "pitch": 0.05381418767278845,
+          "rotation": 0,
+          "target": "2-gabinet-2"
+        },
+        {
+          "yaw": -0.045986622884120365,
+          "pitch": 0.029609057615150647,
+          "rotation": 0,
+          "target": "1-gabinet-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.818890254164244,
+          "pitch": -0.13389125074368735,
+          "title": "Wejście/wyjście",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.4447962739590405,
+          "pitch": -0.049868686565856635,
+          "title": "Ukryty aneks kuchenny",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-gabinet-1",
+      "name": "Gabinet 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.3660189344491052,
+        "pitch": 0.18482801549380312,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1264398597179968,
+          "pitch": -0.0018871733489049802,
+          "rotation": 0,
+          "target": "0-poczekalniaprzedpokj"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-gabinet-2",
+      "name": "Gabinet 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.1797703264356816,
+        "pitch": 0.5259966825079196,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.077989735966943,
+          "pitch": 0.06168638320496278,
+          "rotation": 0,
+          "target": "0-poczekalniaprzedpokj"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-toaleta",
+      "name": "Toaleta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5020109279861877,
+        "pitch": -0.11493523828024976,
+        "fov": 1.308072473761003
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.806529983462628,
+          "pitch": -0.025755795693047645,
+          "rotation": 0,
+          "target": "0-poczekalniaprzedpokj"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Gabinet psychologiczny",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
